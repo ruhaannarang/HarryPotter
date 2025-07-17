2c1dbac2555509a69e388e858a5ccac251c794f9
Ruhaan Narang <techieruhaan@gmail.com>
2025-07-17 22:25:36 +0530

    Created Random cats fact generator

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const URL="https://dogapi.dog/api/v2/breeds";
+const URL="https://meowfacts.herokuapp.com/?count=1";
 let funktion= async () => {
     let dogs= await fetch(URL);
     let dog= await dogs.json();
-    console.log(dog.data[0])
+    console.log(dog.data)
+    console.log(dog.data[2])
+
+setTimeout(() => {
+    document.body.querySelector(".fact").innerText=dog.data
+}, 1000);
 }
 funktion()
+
+document.body.querySelector(".opt").addEventListener("click",function (){
+    let fact=document.body.querySelector(".fact")
+    fact.style.display='block';
+}
+)
